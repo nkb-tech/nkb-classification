d2db52859d15ffcd5cbeb35272f88e2819996f87
Dentikka <shveikin.da@phystech.edu>
2023-11-02 13:56:50 +0300

    fixed model code logging

--- nkb_classification/train.py
+++ nkb_classification/train.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from collections import defaultdict
@@ -295,7 +296,8 @@ def main():
     criterion = get_loss(cfg.criterion, cfg.device)
     experiment = get_experiment(cfg.experiment)
     experiment.log_code(cfg_file)
-    experiment.log_code('./model.py')
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    experiment.log_code(os.path.join(dir_path, './model.py'))
     train(model,
           train_loader,
           val_loader,
